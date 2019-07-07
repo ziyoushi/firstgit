0cd118aff9183fd333d04327c946f0d03ff09eb2
changchen <doublechang123@163.com>
2019-07-07 19:41:15 +0800

    hello world

---
+++ src/main/java/com/atguigu/first/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.atguigu.first.controller;
+
+/**
+ * @author Administrator
+ * @create 2019-07-07 19:38
+ */
+public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
